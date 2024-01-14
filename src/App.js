f7b34bd1d39a930abf4a1f943641993efd4bb691
codeninja <adarsha.pau@gmail.com>
2024-01-14 12:57:47 +0545

    Textcalc

--- src/App.js
+++ src/App.js
@@ -55,8 +55,8 @@ function App() {
       <Alert alert={alert} />
       <div className="container my-3">
         <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/ada" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />} />
+          <Route path="/about" element={<About  mode={mode} />} />
+          <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />} />
         </Routes>
       </div>
     </Router>
--- src/components/About.js
+++ src/components/About.js
@@ -1,37 +1,40 @@
 import React, {useState} from 'react'
 
-export default function About() {
+export default function About(props) {
 
-    const [myStyle, setMyStyle] = useState({
-        color: 'black',
-        backgroundColor: 'white'
-    })
-    const [btntext, setBtnText] = useState("Enable Dark Mode")
-    
-    const toggleStyle = ()=>{
-        if(myStyle.color === 'black'){
-            setMyStyle({ 
-                color: 'white',
-                backgroundColor: 'black',
-                border: '1px solid white'
-                
-            })
-            setBtnText("Enable Light Mode")
-        }
-        else{
-            setMyStyle({ 
-                color: 'black',
-                backgroundColor: 'white'
-            })
-            setBtnText("Enable Dark Mode");
-        }
+    // const [myStyle, setMyStyle] = useState({
+    //     color: 'black',
+    //     backgroundColor: 'white'
+    // })
+    // const [btntext, setBtnText] = useState("Enable Dark Mode")
+    let myStyle={
+        color:props.mode=='dark'?'white':'black',
+        backgroundColor:props.mode=='dark'?'#0000001c':'white',
     }
+    // const toggleStyle = ()=>{
+    //     if(myStyle.color === 'black'){
+    //         setMyStyle({ 
+    //             color: 'white',
+    //             backgroundColor: 'black',
+    //             border: '1px solid white'
+                
+    //         })
+    //         setBtnText("Enable Light Mode")
+    //     }
+    //     else{
+    //         setMyStyle({ 
+    //             color: 'black',
+    //             backgroundColor: 'white'
+    //         })
+    //         setBtnText("Enable Dark Mode");
+    //     }
+    // }
     
     return (
         <div className="container" style={myStyle}>
             <h1 className="my-3">About Us</h1>
             <div className="accordion" id="accordionExample">
-                <div className="accordion-item">
+                <div  style={myStyle} className="accordion-item">
                     <h2 className="accordion-header" id="headingOne">
                     <button className="accordion-button" type="button" style={myStyle} data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                         Accordion Item #1
@@ -55,7 +58,7 @@ export default function About() {
                     </div>
                     </div>
                 </div>
-                <div className="accordion-item">
+                <div className="accordion-item" style={myStyle}>
                     <h2 className="accordion-header" id="headingThree">
                     <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                         Accordion Item #3
@@ -68,9 +71,9 @@ export default function About() {
                     </div>
                 </div>
             </div>
-            <div className="container my-3">
+            {/* <div className="container my-3">
                 <button onClick={toggleStyle} type="button" className="btn btn-primary">{btntext}</button>
-            </div>
+            </div> */}
         </div>
     )
 }
